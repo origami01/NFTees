74a221134f7dd399f8d33d026afd1345a6505514
origami01 <dicksonobenamukama@gmail.com>
2022-07-17 00:43:39 +0300

    Commit message

---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+//import ethers from Hardhat package
+const { ethers } = require ("hardhat");
+
+async function main() {
+
+  const nftContract = await ethers.getContractFactory("GameItem");
+
+  //here we deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
+
+  //print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+//Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+  
